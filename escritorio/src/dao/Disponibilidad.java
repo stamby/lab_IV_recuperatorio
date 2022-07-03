e11af65838eb94ac01c7107f37bf13a5ad56018a
Bruno <stamby@github.com>
2022-07-03 18:52:12 -0300

    Primer commit

---
+++ escritorio/src/dao/Disponibilidad.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.ArrayList;
+
+import entidad.Habitacion;
+
+public interface HabitacionDao {
+	public boolean Agregar(Habitacion tipo);
+	public ArrayList<Habitacion> ListarTodo();
+	public Habitacion ObtenerObjeto(int id);
+	public boolean Eliminar(int id);
+	public boolean Modificar(Habitacion habitacion);
+}
---
+++ escritorio/src/entidad/Disponibilidad.java
@@ -0,0 +1,80 @@
+package entidad;
+
+public class Disponibilidad {
+    private int numero;
+    private int capacidad_personas;
+    private int camas;
+    private boolean estado;
+    private boolean television;
+    private boolean calefaccion;
+    private boolean wifi;
+    private boolean frigobar;
+    private boolean vista_panoramica;
+
+    public int get_numero() {
+        return numero;
+    }
+    public int get_capacidad_personas() {
+        return capacidad_personas;
+    }
+    public int get_camas() {
+        return camas;
+    }
+    public boolean get_estado() {
+        return estado;
+    }
+    public boolean get_estado() {
+        return estado;
+    }
+    public boolean get_television() {
+        return television;
+    }
+    public boolean get_calefaccion() {
+        return calefaccion;
+    }
+    public boolean get_wifi() {
+        return wifi;
+    }
+    public boolean get_frigobar() {
+        return frigobar;
+    }
+    public boolean get_vista_panoramica() {
+        return vista_panoramica;
+    }
+    public void set_numero() {
+        this.numero = numero;
+    }
+    public void set_capacidad_personas() {
+        this.capacidad_personas = capacidad_personas;
+    }
+    public void set_camas() {
+        this.camas = camas;
+    }
+    public void set_estado() {
+        this.estado = estado;
+    }
+    public void set_estado() {
+        this.estado = estado;
+    }
+    public void set_television() {
+        this.television = television;
+    }
+    public void set_calefaccion() {
+        this.calefaccion = calefaccion;
+    }
+    public void set_wifi() {
+        this.wifi = wifi;
+    }
+    public void set_frigobar() {
+        this.frigobar = frigobar;
+    }
+    public void set_vista_panoramica() {
+        this.vista_panoramica = vista_panoramica;
+    }
+    @Override
+    public String toString() {
+        return "";
+    }
+
+}
+
